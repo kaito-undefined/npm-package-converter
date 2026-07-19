8e70277c5fa6b916e989f222487b16d264164463
kaito-undefined <kaito61y@gmail.com>
2026-07-19 08:03:26 +0300

    readied project for build and resized+renamed icon

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'npm.png'),
+    icon: path.join(__dirname, 'npm.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
